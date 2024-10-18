be2e77f3ae6f43a711460a91cba6ae6a92366755
ext-h-asano <asano@ext.cx>
2024-10-18 14:22:25 +0900

    test

--- index_answer.js
+++ index_answer.js
@@ -111,7 +111,7 @@ function startServerConnection(localId, remoteId) {
 		sc.close();
 	}
 	// サーバー接続の開始
-	sc = new WebSocket('wss://' + '52.194.235.65' + ':' + sslPort + '/');
+	sc = new WebSocket('wss://' + 'signaling.android-vpn.com' + ':' + sslPort + '/');
 	sc.onmessage = gotMessageFromServer;
 	sc.onopen = function(event) {
 		console.log('[startServerConnection] WebSocket connection opened.');
